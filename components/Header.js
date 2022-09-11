4a48ffd2d0119d830fba37230a1517f58a7e4988
Stephen Kaczmarowski <stephenkaczmarowski@Stephens-MacBook-Pro.local>
2022-09-11 09:59:44 -0700

    added header and fixed tailwind config

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,23 @@
+import Image from 'next/image';
 import React from 'react';
+import HeaderIcon from './HeaderIcon';
+import { HomeIcon, UserIcon, PhoneIcon, InformationCircleIcon } from '@heroicons/react/solid'
 
 const Header = () => {
     return (
         <div>
-            Header
+            <div className=''>
+                <HeaderIcon Icon={HomeIcon} title="HOME" />
+                <HeaderIcon Icon={UserIcon} title="CONTACT" />
+                <HeaderIcon Icon={PhoneIcon} title="ACCOUNT" />
+                <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
+            </div>
+            <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
+                width={100}
+                height={100}
+                alt="logo"
+            />
         </div>
     );
 }
