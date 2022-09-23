aff156c4a968aeb03686d741e7a0e29b7d321e49
Stephen Kaczmarowski <stephenkaczmarowski@Stephens-MacBook-Pro.local>
2022-09-23 13:55:48 -0700

    create server side function and pass to client side

--- components/Results.js
+++ components/Results.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Results = () => {
+export default function Results({results}) {
     return (
         <div>
-            Results
+            {results.map((result) => (
+                <h1 key={result.title}>{result.title}</h1>
+            ))}
         </div>
     );
 }
 
-export default Results;
